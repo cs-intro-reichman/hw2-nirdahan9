d372e7a76926dac1d0659a7f80cb3f8430f662d3
nirdahan9 <nir.dahan01@post.runi.ac.il>
2024-11-12 22:18:51 +0200

    12/11 22:18

--- Cheers.java
+++ Cheers.java
@@ -2,5 +2,33 @@
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            int index = 0;
+            while(index < args.length-1) {
+                if((args[index].charAt(0)=='A')||(args[index].charAt(0)=='E')
+                ||(args[index].charAt(0)=='F')||(args[index].charAt(0)=='H')
+                ||(args[index].charAt(0)=='I')||(args[index].charAt(0)=='L')
+                ||(args[index].charAt(0)=='M')||(args[index].charAt(0)=='N')
+                ||(args[index].charAt(0)=='O')||(args[index].charAt(0)=='R')
+                ||(args[index].charAt(0)=='S')||(args[index].charAt(0)=='X')) {
+                        System.out.println("Give me an "+args[index]+": "+args[index]+"!");
+                }
+                else {
+                        System.out.println("Give me a "+args[index]+": "+args[index]+"!");
+                }
+                index++;
+            }
+            System.out.println("What does that spell?");
+            int count = 0;
+            index = 0;
+            String name = "";
+            while(index < args.length-1) {
+                name += args[index];
+                index++;
+            }
+            while(count < Integer.parseInt(args[args.length-1])) {
+                System.out.println(name + " ! ! !");
+                count++;
+            }
+
         }
 }
