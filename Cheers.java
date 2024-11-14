551a9aa050495a1c21e5b44c78c06c6122c56ea9
nirdahan9 <nir.dahan01@post.runi.ac.il>
2024-11-14 08:59:51 +0200

    14.11 09:00

--- Cheers.java
+++ Cheers.java
@@ -5,6 +5,7 @@ public class Cheers {
             int index = 0;
             char ch = args[0].toUpperCase().charAt(index);
             while(index < args[0].length()) {
+                ch = args[0].toUpperCase().charAt(index);
                 if((ch=='A')||(ch=='E')||(ch=='F')||(ch=='H')||(ch=='I')||(ch=='L')
                 ||(ch=='M')||(ch=='N')||(ch=='O')||(ch=='R')||(ch=='S')||(ch=='X')) {
                         System.out.println("Give me an "+ch+":  "+ch+"!");
@@ -14,10 +15,9 @@ public class Cheers {
                         System.out.println("Give me a "+ch+":  "+ch+"!");
                 }
                 index++;
-                ch = args[0].toUpperCase().charAt(index);
             }
             System.out.println("What does that spell?");
-            for(int i = 0; i < args[0].length() ; i ++) {
+            for(int i = 0; i < Integer.parseInt(args[1]) ; i ++) {
                 System.out.println(args[0] + " ! ! !");
             }
 
