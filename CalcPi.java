dd58748924af880f3edc5da4b574329e5d763131
nirdahan9 <nir.dahan01@post.runi.ac.il>
2024-11-15 08:42:58 +0200

    15.11 08:42

--- CalcPi.java
+++ CalcPi.java
@@ -1,28 +1,30 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
 		double result = 1.0;
 		double toReduce = 3.0;
 		double toAdd = 5.0;
-		int toAddTimes = 0, toReduceTimes = 0;
-		if(Integer.parseInt(args[0])%2==0) {
-			toAddTimes = ((Integer.parseInt(args[0])-1)/2);
-			toReduceTimes = (Integer.parseInt(args[0]))/2;
-		}
-		else {
-			toAddTimes = ((Integer.parseInt(args[0]))/2);
-			toReduceTimes = ((Integer.parseInt(args[0]))/2);
-		}
-		for(int i = 0 ; i < toAddTimes ; i ++) {
-			result += (1.0/toAdd);
-			toAdd += 4.0;
-		}
-		for(int i = 0 ; i < toReduceTimes ; i ++) {
+		int times = Integer.parseInt(args[0]);
+		if(times %2 ==0) { // checking the case that the number is even
+			times = (times-2)/2 ;
+			for(int i = 0 ; i < times ; i++) {
+				result -= (1.0/toReduce);
+				toReduce += 4.0;
+				result += (1.0/toAdd);
+				toAdd += 4.0;
+			}
 			result -= (1.0/toReduce);
-			toReduce += 4.0;
 		}
-		result*=4.0;
+		else { // checking the case that the times number is odd
+			times /= 2;
+			for(int i = 0 ; i < times ; i++) {
+				result -= (1.0/toReduce);
+				toReduce += 4.0;
+				result += (1.0/toAdd);
+				toAdd += 4.0;
+			}
+		}
+		result *= 4.0;
 		System.out.println("pi according to Java: " + Math.PI);
 		System.out.println("pi, approximated:     " + result);
 	}
